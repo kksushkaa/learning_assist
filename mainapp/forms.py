72e1e45a59ba6d8e44805c9a640dfbe2536322cb
kksushkaa <ksenya.taldykina@gmail.com>
2025-03-03 20:44:05 +0300

    + err

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -12,50 +12,76 @@ class UserLoginForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={"class": "form-control ", "placeholder": "Введите логин"}
-        )
+        ),
+        error_messages={'required': 'Пожалуйста, введите имя пользователя'}
     )
     password = forms.CharField(
-        widget=forms.TextInput(
+        widget=forms.PasswordInput(
             attrs={"class": "form-control", "placeholder": "Введите пароль"}
-        )
+        ),
+        error_messages={'required': 'Пожалуйста, подтвердите пароль'}
     )
 
     class Meta:
         model = MyUser
         fields = ["username", "password"]
-
+    
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.error_messages.update(
+            {
+            'invalid_login': 'Пожалуйста, введите правильные имя пользователя и пароль.',
+            'inactive': 'Этот аккаунт неактивен.'
+        
+        }) 
 
 class UserRegForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={"class": "form-control ", "placeholder": "Введите логин"}
-        )
+        ), 
+        error_messages={'required': 'Пожалуйста, введите имя пользователя',
+                        'unique': 'Пользователь с таким именем уже существует'}
     )
     email = forms.CharField(
         widget=forms.EmailInput(
             attrs={"class": "form-control ", "placeholder": "Введите почту "}
-        )
+        ),
+        error_messages={'required': 'Пожалуйста, введите адрес электронной почты',
+                        'invalid': 'Введите корректный адрес электронной почты'}
     )
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={"class": "form-control ", "placeholder": "Введите ваше имя "}
-        )
+        ),
+        error_messages={'required': 'Пожалуйста, введите ваше имя'}
     )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={"class": "form-control ", "placeholder": "Введите пароль "}
-        )
+        ),
+        error_messages={'required': 'Пожалуйста, введите пароль'}
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={"class": "form-control ", "placeholder": "Введите пароль ещё раз "}
-        )
+        ),
+        error_messages={'required': 'Пожалуйста, подтвердите пароль',
+                        'password_mismatch': 'Пароли не совпадают'}
     )
 
     class Meta:
         model = MyUser
         fields = ["username", "email", "first_name", "password1", "password2"]
 
+    def __init__(self, *args, **kwargs):
+        super(UserRegForm, self).__init__(*args, **kwargs)
+        self.error_messages.update({
+            'password_too_short': 'Пароль слишком короткий. Он должен содержать как минимум 8 символов.',
+            'password_too_common': 'Пароль слишком простой.',
+            'password_entirely_numeric': 'Пароль не может состоять только из цифр.',
+            'password_mismatch': 'Пароли не совпадают'
+        }) 
 
 class TaskAddForm(ModelForm):
     # настройка формы
@@ -96,6 +122,7 @@ class TaskAddForm(ModelForm):
         if commit:
             instance.save()
         return instance
+    
 
 
 class TaskEditForm(ModelForm):
